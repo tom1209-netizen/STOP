c8d335bae304d09ae21dfcd4a2727c7ef4223445
Lee Khan <lephuckhang0@gmail.com>
2025-08-11 01:14:06 +0900

    Feat: first commit

---
+++ dataloaders/data_dataloaders.py
@@ -0,0 +1,63 @@
+# coding=utf-8
+import torch
+from torch.utils.data import DataLoader
+from dataloaders.dataloader_msrvtt_retrieval import MSRVTT_DataLoader
+from dataloaders.dataloader_msrvtt_retrieval import MSRVTT_TrainDataLoader
+
+def dataloader_msrvtt_train(args, tokenizer):
+    # print("args.features_path:")
+    # print(args.features_path)
+    # input()
+    msrvtt_dataset = MSRVTT_TrainDataLoader(
+        csv_path=args.train_csv,
+        json_path=args.data_path,
+        features_path=args.features_path,
+        max_words=args.max_words,
+        feature_framerate=args.feature_framerate,
+        tokenizer=tokenizer,
+        max_frames=args.max_frames,
+        unfold_sentences=args.expand_msrvtt_sentences,
+        frame_order=args.train_frame_order,
+        slice_framepos=args.slice_framepos,
+        lmdb_dataset=args.lmdb_dataset
+    )
+    if torch.distributed.is_available() and torch.distributed.is_initialized():
+        train_sampler = torch.utils.data.distributed.DistributedSampler(msrvtt_dataset)
+    else:
+        train_sampler = None
+
+    dataloader = DataLoader(
+        msrvtt_dataset,
+        batch_size=args.batch_size,
+        num_workers=args.num_thread_reader,
+        pin_memory=False,
+        shuffle=(train_sampler is None),
+        sampler=train_sampler,
+        drop_last=True,
+    )
+
+    return dataloader, len(msrvtt_dataset), train_sampler
+
+def dataloader_msrvtt_test(args, tokenizer, subset="test"):
+    msrvtt_testset = MSRVTT_DataLoader(
+        csv_path=args.val_csv,
+        features_path=args.features_path,
+        max_words=args.max_words,
+        feature_framerate=args.feature_framerate,
+        tokenizer=tokenizer,
+        max_frames=args.max_frames,
+        frame_order=args.eval_frame_order,
+        slice_framepos=args.slice_framepos,
+        lmdb_dataset=args.lmdb_dataset
+    )
+    dataloader_msrvtt = DataLoader(
+        msrvtt_testset,
+        batch_size=args.batch_size_val,
+        num_workers=args.num_thread_reader,
+        shuffle=False,
+        drop_last=False,
+    )
+    return dataloader_msrvtt, len(msrvtt_testset)
+
+DATALOADER_DICT = {}
+DATALOADER_DICT["msrvtt"] = {"train":dataloader_msrvtt_train, "val":dataloader_msrvtt_test, "test":None}
---
+++ scripts/msrvtt.sh
@@ -0,0 +1,100 @@
+#!/bin/bash
+export CUDA_VISIBLE_DEVICES=0,1
+echo "Using local machine for training"
+
+
+
+group=group2-2
+
+
+# dataset
+dataset=msrvtt
+fps=3
+
+DATA_PATH=/kaggle/input/msrvtt
+data_path=${DATA_PATH}/annotation/MSRVTT_data.json
+train_csv=${DATA_PATH}/annotation/MSRVTT_train.9k.csv
+
+# training-9k
+val_csv=${DATA_PATH}/annotation/MSRVTT_JSFUSION_test.csv
+features_path=${DATA_PATH}/compressed_videos/
+pretrained_dir=/kaggle/input/stop-model-implementation/modules
+
+# train or eval
+do_train=1
+do_eval=0
+
+
+# learning strategies
+pretrained_clip_name=ViT-B/32
+lr=1e-3
+coef_lr=5e-4
+wd=0.2
+epochs=5
+optim=AdamW
+max_words=32
+max_frames=12
+temperature_new=1.0
+resume=None
+load_from_pretrained=0
+batch_size=16           # single GPU batch size
+batch_size_val=16
+num_workers=4
+n_display=50            # log per n_display
+precision=amp
+
+freeze_clip=1
+time_embedding=0
+
+shared_latent_space=transformer
+
+# distributed training
+init_method='tcp://127.0.0.1:6010'
+
+
+
+
+current_datetime=$(TZ="Asia/Shanghai" date +"%Y-%m-%d-%H:%M:%S")
+model_dir=/kaggle/working/logs/${current_datetime}_${dataset}_STOP
+echo "The model dir is ${model_dir}"
+# CUDA_LAUNCH_BLOCKING=1
+python  main.py \
+        --do_train ${do_train} \
+        --do_eval ${do_eval} \
+        --num_thread_reader ${num_workers} \
+        --epochs ${epochs} \
+        --batch_size ${batch_size} \
+        --n_display ${n_display} \
+        --train_csv ${train_csv} \
+        --val_csv ${val_csv} \
+        --data_path ${data_path} \
+        --features_path ${features_path} \
+        --output_dir ${model_dir} \
+        --optim ${optim} \
+        --lr ${lr} \
+        --coef_lr ${coef_lr} \
+        --wd ${wd} \
+        --max_words ${max_words} \
+        --max_frames ${max_frames} \
+        --batch_size_val ${batch_size_val} \
+        --datatype ${dataset} \
+        --expand_msrvtt_sentences  \
+        --feature_framerate ${fps} \
+        --freeze_layer_num 12  \
+        --slice_framepos 2 \
+        --loose_type \
+        --linear_patch 2d \
+        --sim_header meanP \
+        --pretrained_clip_name ${pretrained_clip_name} \
+        --precision ${precision} \
+        --init_method ${init_method} \
+        --pretrained_dir ${pretrained_dir} \
+        --freeze_clip ${freeze_clip} \
+        --time_embedding ${time_embedding} \
+        --resume ${resume} \
+        --load_from_pretrained ${load_from_pretrained} \
+        --shared_latent_space ${shared_latent_space} \
+        --temporal_prompt ${group}
+
+
+echo "Training Finished!!!"
